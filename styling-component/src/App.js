dbedefbc257117a950a252c00227390996cd9690
yuum <dyalsla0105@gmail.com>
2022-06-30 15:22:30 +0900

    React

---
+++ styling-component/src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import SassComponent from "./components/SassComponent";
+
+function App() {
+  return <SassComponent></SassComponent>;
+}
+
+export default App;
