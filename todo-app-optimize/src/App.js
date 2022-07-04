7f9cc08f43a0df3fd015587dd678f81cba055cab
yuum <dyalsla0105@gmail.com>
2022-07-04 11:02:45 +0900

    React

--- todo-app-optimize/src/App.js
+++ todo-app-optimize/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useRef, useCallback } from 'react';
+import { useState, useRef, useCallback, useReducer } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
@@ -16,8 +16,23 @@ function createBultTodos() {
   return array;
 }
 
+function todoReducer(todos, action) { //action에 새로 생긴 todo를 추가
+  switch (action.type) {
+    case: 'INSERT':
+      return todos.concat(action.todo); 
+    case 'REMOVE':
+      return todos.filter(todo => todo.id !== action.id);
+    case 'CHECKD':
+      return todos.map(todo => todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,);
+    default:
+      return todos;
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState(createBultTodos);
+  //초기상태를 undefind로 설정하고 세 번째 파라미터에 초기상태를 만들어주는 함수를 넣어주면 
+  //처음 렌더링될 때만 초기상태를 만들어주는 함수를 호출
+  const [todos, dispath] = useReducer(todoReducer, undefined, createBultTodos);
 
   const nextId = useRef(2501);
 
@@ -28,7 +43,7 @@ function App() {
         text: text,
         checked: false,
       };
-      setTodos(todos.concat(todo));
+      dispath({ type: 'INSERT', todo }); //새로 만들어진 todo 객체를 dispath에 넣어줌
       nextId.current += 1;
     },
     [todos],
@@ -36,7 +51,7 @@ function App() {
 
   const onRemove = useCallback(
     (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
+      dispath({ type: 'REMOVE', id });
     },
     [todos],
   );
@@ -45,6 +60,8 @@ function App() {
     (id) => {
       setTodos(
         todos.map((todo) =>
+          //불변성을 지키기위해 원본 todo의 값을 변경하지 않고
+          //todo를 복제한 새로운 todo객체를 만들어서 return
           todo.id === id ? { ...todo, checked: !todo.checked } : todo,
         ),
       );
