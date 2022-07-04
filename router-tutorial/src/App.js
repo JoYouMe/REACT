8d36c93d09714841a73e5f9ebc00fe56dd67d0a6
yuum <dyalsla0105@gmail.com>
2022-07-04 14:45:13 +0900

    React

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Profile from "./components/Profile";
+import Articles from "./components/Articles";
+import Article from "./components/Article";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/profile/:name" element={<Profile />}></Route>
+      <Route path="/articles" element={<Articles />}></Route>
+      <Route path=":id" element={<Article />}></Route>
+    </Routes>
   );
 }
 
