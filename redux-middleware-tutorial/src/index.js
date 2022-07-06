7a2fe4ae147e7072a533ebd92b87cf5c8c1aab1a
yuum <dyalsla0105@gmail.com>
2022-07-06 15:45:52 +0900

    React

--- redux-middleware-tutorial/src/index.js
+++ redux-middleware-tutorial/src/index.js
@@ -4,10 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { legacy_createStore as createStore } from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import rootReducer from "./modules";
+import { createLogger } from "redux-logger";
+import ReduxThunk from "redux-thunk";
 
-const store = createStore(rootReducer);
+const logger = createLogger();
+
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -16,7 +20,4 @@ root.render(
   </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
